d7ac383a5bd41650d660cbc295364d38861b0a30
Suki <suki@local>
2026-08-01 15:56:56 +0800

    init: Suki reading room PWA

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE = 'suki-reading-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './bg-liquid-glass.jpg',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) {
+      return c.addAll(ASSETS);
+    }).catch(function () {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) {
+        if (k !== CACHE) return caches.delete(k);
+      }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  // 只缓存同源 GET 请求；OpenLibrary 等外部请求走网络
+  if (e.request.method !== 'GET') return;
+  var url = new URL(e.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  e.respondWith(
+    caches.match(e.request).then(function (hit) {
+      if (hit) return hit;
+      return fetch(e.request).then(function (res) {
+        if (res && res.ok && res.type === 'basic') {
+          var copy = res.clone();
+          caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+        }
+        return res;
+      }).catch(function () { return caches.match('./'); });
+    })
+  );
+});
